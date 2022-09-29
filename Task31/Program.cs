5774a3c4a6b55e803ca1882ba360ac627a67a071
Oksana <oksana.gaech@mail.ru>
2022-09-29 10:57:34 +0300

    add Task31

---
+++ Task31/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    var rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray (int[] array)
+{
+
+}
+
+int [] GetSumPositiveNegativeElem(int[] array)
+{
+    int sumPositive = default;
+    int sumNegative = default;
+    for (int i = 0, i < array.Length, i++)
+    {
+        if (array[i] < 0) sumNegative += array[i]; //sumNegative =sumNegative =+ array[i]    }
+        else sumPositive += array[i];// sumPositive = sumPositive + array[i]
+
+
+}
