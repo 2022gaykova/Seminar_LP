8b38452634a676d23cd5a0878a6d69f493c8b1ec
Oksana <oksana.gaech@mail.ru>
2022-10-03 10:51:49 +0300

    add Task39

---
+++ Task39/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    var rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray (int[] array)
+
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length-1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+void ReverseArray (int[] array)
+{
+    int size = array.Length;
+    int index1 = 0;
+    int index2 = size - 1;
+    while (index1 < index2)
+    {
+      int obj = array[index1];
+      array[index1] = array[index2];
+      array[index2] =  obj;
+
+      index1++;
+      index2--; 
+    }
+}
+
+int[] arr = CreateArrayRndInt(8, -9, 9);
+PrintArray(arr);
+ReverseArray(arr);
+PrintArray(arr);
