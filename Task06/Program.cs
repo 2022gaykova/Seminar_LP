a892b8c0cb767dad75e298ae71eedd9008a609da
Oksana <oksana.gaech@mail.ru>
2022-10-05 18:34:41 +0300

    add Task06

---
+++ Task06/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 6. Напишите программу, которая на входе принимает число
+// и выдает, является ли число четным
+// 4 -> да
+// 5 -> нет
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0) Console.Write($"{number}-> да"); 
+else Console.Write($"{number}-> нет"); 
