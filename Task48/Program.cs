24c4c8927898985f014c2208b729b55aa78d6826
Oksana <oksana.gaech@mail.ru>
2022-10-06 17:01:17 +0300

    add Task48

---
+++ Task48/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 48.
+
+int[,] CreateMatrixSumIndex(int rows,int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < rows; i++) //rows (0)
+    { 
+       for (int j = 0; j < columns; j++) //columns (1)
+        {
+        matrix[i, j] = i + j;    
+        }
+    }
+    return matrix;
+}
+void PrintMatrix (int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            Console.Write("|");
+            for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 4}, ");
+                    else Console.Write($"{matrix[i, j], 4}");
+                }
+                Console.WriteLine("|");
+        }
+} 
+
+int [,] array2D = CreateMatrixSumIndex(3, 4);
+PrintMatrix(array2D);
