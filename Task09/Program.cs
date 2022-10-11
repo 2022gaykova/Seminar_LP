705aac7dc897ed9374b59a8f173a0eddd213f3b7
Oksana <oksana.gaech@mail.ru>
2022-10-11 10:54:09 +0300

    add Task11

---
+++ Task09/Program.cs
@@ -0,0 +1,17 @@
+﻿// 9. Напишите программу, которая выводит случайное
+// 
+
+// 78 -> 8
+// 85 -> 8
+// 88 -> 
+
+int number = 99; // new Random().Next(10, 100);
+Console.WriteLine($"Случайное число из отрезка 10 - 99 => {number}");
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+if (firstDigit == secondDigit) System.Console.WriteLine("Цифры равны");
+else if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа {number} => {firstDigit}");
+else Console.WriteLine($"Наибольшая цифра числа {number} => {secondDigit}");
+
+
+int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit; // бинарный  оперант
---
+++ Task11/Program.cs
@@ -0,0 +1,14 @@
+﻿// 11.Напишите программу, которая выводит случайное 3хзначное
+//  число и удаляет 2ю цифру этого
+// 456 => 46
+
+int number = new Random().Next(100,1000);
+int RemoveSecondDigit( int num)
+{
+    int thirdDigit = num % 10;
+    int firstDigit = num / 100;
+    return firstDigit * 10 + thirdDigit;
+}
+
+int result = RemoveSecondDigit(number);
+Console.WriteLine($"Итоговое число от {number} => {result}");
